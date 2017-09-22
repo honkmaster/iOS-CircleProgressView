a5bab5ed43480825d7b9d904b80e9d7023e85370
Tobias Tiemerding <tobias@tiemerding.com>
2017-09-22 14:40:36 +0200

    Update CircleProgressView.swift
    Updated to Swift 4.0 and removed deprecated warning

--- ProgressView/CircleProgressView.swift
+++ ProgressView/CircleProgressView.swift
@@ -131,22 +131,22 @@ import UIKit
         let progressRect: CGRect = CGRect(x: innerRect.minX, y: innerRect.minY, width: innerRect.width, height: innerRect.height)
         let center = CGPoint(x: progressRect.midX, y: progressRect.midY)
         let radius = progressRect.width / 2.0
-        let startAngle:CGFloat = clockwise ? CGFloat(-internalProgress * M_PI / 180.0) : CGFloat(constants.twoSeventyDegrees * M_PI / 180)
-        let endAngle:CGFloat = clockwise ? CGFloat(constants.twoSeventyDegrees * M_PI / 180) : CGFloat(-internalProgress * M_PI / 180.0)
+        let startAngle:CGFloat = clockwise ? CGFloat(-internalProgress * .pi / 180.0) : CGFloat(constants.twoSeventyDegrees * .pi / 180)
+        let endAngle:CGFloat = clockwise ? CGFloat(constants.twoSeventyDegrees * .pi / 180) : CGFloat(-internalProgress * .pi / 180.0)
  
         progressPath.addArc(withCenter: center, radius:radius, startAngle:startAngle, endAngle:endAngle, clockwise:!clockwise)
         let r = radius - trackWidth * 0.5
         if roundedCap {
             let capCenter = CGPoint(x: center.x + r * cos(endAngle), y: center.y + r * sin(endAngle))
         
-            progressPath.addArc(withCenter: capCenter, radius: trackWidth * 0.5, startAngle: endAngle, endAngle: endAngle + CGFloat(M_PI), clockwise: !clockwise)
+            progressPath.addArc(withCenter: capCenter, radius: trackWidth * 0.5, startAngle: endAngle, endAngle: endAngle + .pi, clockwise: !clockwise)
         }
         progressPath.addArc(withCenter: center, radius:radius-trackWidth, startAngle:endAngle, endAngle:startAngle, clockwise:clockwise)
         
         if roundedCap {
         let capCenter = CGPoint(x: center.x + r * cos(startAngle), y: center.y + r * sin(startAngle))
         
-            progressPath.addArc(withCenter: capCenter, radius: trackWidth * 0.5, startAngle: startAngle, endAngle: startAngle + CGFloat(M_PI), clockwise: clockwise)
+            progressPath.addArc(withCenter: capCenter, radius: trackWidth * 0.5, startAngle: startAngle, endAngle: startAngle + .pi, clockwise: clockwise)
         }
         progressPath.close()
         
@@ -198,7 +198,7 @@ import UIKit
 
     //MARK: - CADisplayLink Tick
 
-    internal func displayLinkTick() {
+    @objc internal func displayLinkTick() {
 
         let renderTime = refreshRate.isZero ? displayLink!.duration : refreshRate
 
